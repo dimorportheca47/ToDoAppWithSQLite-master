beee40c5d7f1b9d3695361e112f6a6686557142a
Shuhei Fukami <1384371t@stu.kobe-u.ac.jp>
2019-10-08 18:51:08 +0900

    ToDoのメニュー画面のレイアウトを作成

---
+++ app/src/main/java/com/example/viewsample/MenuTodo.java
@@ -0,0 +1,14 @@
+package com.example.viewsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuTodo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_todo);
+    }
+}
