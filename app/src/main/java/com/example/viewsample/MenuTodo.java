e391b5595cf7c39898e174056360c3f733a3262d
Shuhei Fukami <1384371t@stu.kobe-u.ac.jp>
2019-10-11 17:28:42 +0900

    戻るボタン、ToDo名などをアクションバーに統一

--- app/src/main/java/com/example/viewsample/MenuTodo.java
+++ app/src/main/java/com/example/viewsample/MenuTodo.java
@@ -1,9 +1,11 @@
 package com.example.viewsample;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MenuTodo extends AppCompatActivity {
 
@@ -11,8 +13,25 @@ public class MenuTodo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_todo);
+
+        //前画面からの情報を取得
+        Intent intent = getIntent();
+        String name = intent.getStringExtra("name");
+        String detail = intent.getStringExtra("detail");
+        String timeStamp = intent.getStringExtra("timeStamp");
+        String isStar = intent.getStringExtra("isStar");
+        String isArchive = intent.getStringExtra("isArchive");
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setTitle(name);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
-    Intent intent = getIntent();
+
 
 }
